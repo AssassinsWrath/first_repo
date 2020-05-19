fe126ca15796361d716aa2e6f5938546f404643f
AssassinsWrath <t.perks@live.co.uk>
2020-05-20 00:11:11 +0100

    Update new_install_1.sh

--- new_install_1.sh
+++ new_install_1.sh
@@ -38,7 +38,7 @@ apt install steam-devices
 apt install remmina
 
 # Setup fancy command prompt
-wget https://raw.githubusercontent.com/ChrisTitusTech/scripts/master/fancy-bash-promt.sh
+wget https://raw.githubusercontent.com/AssassinsWrath/first_repo/master/fancy-bash-prompt.sh
 mv fancy-bash-promt.sh .fancy-bash-promt.sh
 echo "source ~/.fancy-bash-promt.sh" >> ~/.bashrc
 echo "neofetch" >> ~/.bashrc
