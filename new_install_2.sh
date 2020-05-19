1f160f037c78383d0d58508485137991226d3892
AssassinsWrath <t.perks@live.co.uk>
2020-05-20 00:20:55 +0100

    Update and rename install2.sh to new_install_2.sh

---
+++ new_install_2.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Symlink for config files
+#mv ~/.config ~/.configOLD
+#ln -s ~/Nextcloud/Config/ ~/.config
+
+# Symlink .minecraft folder so previous save works.
+#ln -s ~/Nextcloud/Minecraft ~/.minecraft
+
+# Setup other Symlinks
+#rm -rf ~/Documents
+#rm -rf ~/Pictures
+#rm -rf ~/Public
+#rm -rf ~/Templates
+#rm -rf ~/Videos
+#ln -s ~/Nextcloud/Documents ~/Documents
+#ln -s ~/Nextcloud/Photos ~/Pictures
+
+# Setup terminal alias
+echo alias update='sudo apt update' >> ~/.bashrc
+echo alias upgrade='sudo apt upgrade -y' >> ~/.bashrc
+
+# Final message
+echo User folders have been configured, the script will now quit.
+ 
+# Exit the script
+exit 0
